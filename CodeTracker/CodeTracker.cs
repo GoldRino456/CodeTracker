ee2c5c8d538beff55ee7414d478efcdf9c03d87d
Ethan E. <eastwoode375@gmail.com>
2025-06-15 18:27:53 -0500

    Database Creation + Initial Table Added

---
+++ CodeTracker/CodeTracker.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeTracker
+{
+    public static class CodeTracker
+    {
+        private static DatabaseManager _databaseManager;
+
+        static void Main()
+        {
+            InitializeCodeTrackerComponents();
+        }
+
+        private static void InitializeCodeTrackerComponents()
+        {
+            _databaseManager = new();
+        }
+    }
+}
---
+++ CodeTracker/DatabaseManager.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System.Configuration;
+using System.Data.SQLite;
+
+namespace CodeTracker
+{
+    internal class DatabaseManager
+    {
+        private readonly string _connectionString;
+
+        //Reusable SQL Commands
+        private readonly string _databaseInitialization = "CREATE TABLE IF NOT EXISTS CodingSessions (id INTEGER PRIMARY KEY AUTOINCREMENT, startTime TEXT, endTime TEXT, duration TEXT)";
+
+        public DatabaseManager()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["SQLiteDB"].ConnectionString;
+            InitializeDB();
+        }
+
+        private void InitializeDB()
+        {
+            ExecuteNonQueryCommand(_databaseInitialization);
+        }
+
+        public void ExecuteNonQueryCommand(string query)
+        {
+            int result;
+
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                result = connection.Execute(query);
+                connection.Close();
+            }
+        }
+    }
+}
